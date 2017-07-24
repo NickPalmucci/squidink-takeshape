bc8883076a595abdb1423bb0f0fed899f87b83da
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-07-24 13:56:03 -0400

    refactor scrollmagic remove offsets, switch to trigger div and add on enter top fix

--- src/javascripts/scroll.js
+++ src/javascripts/scroll.js
@@ -2,22 +2,40 @@ import ScrollMagic from 'scrollmagic';
 
 const controller = new ScrollMagic.Controller();
 
-new ScrollMagic.Scene({duration: 0, offset: 120})
+new ScrollMagic.Scene({duration: 0})
   .setPin('.sideMenu')
   .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement: '.eight', offset: 300})
+new ScrollMagic.Scene({duration: 0})
+  .setPin('.mbSide')
+  .addTo(controller);
+
+const sceneOne = new ScrollMagic.Scene({triggerElement: '#trigger'})
   .setClassToggle('.sideMenu', 'displayMenu')
   .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement: '.eight', offset: 300})
+sceneOne.on('enter', () => {
+  const sideMenu = document.getElementsByClassName('sideMenu')[0];
+  sideMenu.style.top = '2rem';
+});
+
+const sceneTwo = new ScrollMagic.Scene({triggerElement: '#trigger'})
+  .setClassToggle('.mbLogo', 'mbLogoActive')
+  .addTo(controller);
+
+sceneTwo.on('enter', () => {
+  const mbMenu = document.getElementsByClassName('mbSide')[0];
+  mbMenu.style.top = '2rem';
+});
+
+new ScrollMagic.Scene({triggerElement: '#trigger'})
   .setClassToggle('.sideLogoContainer', 'sideLogoActive')
   .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement: '.eight', offset: 300})
+new ScrollMagic.Scene({triggerElement: '#trigger'})
   .setClassToggle('.sideLogoMenu', 'superSide')
   .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement: '.eight', offset: 300})
+new ScrollMagic.Scene({triggerElement: '#trigger'})
   .setClassToggle('.scrollmagic-pin-spacer', 'freePin')
   .addTo(controller);
