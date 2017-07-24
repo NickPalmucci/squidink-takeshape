f30edb9319283f98c99b3cd5f581cb2836d6bf82
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-07-24 13:52:58 -0400

    add mobile menu click handling with temporary nav top fix

--- src/javascripts/nav.js
+++ src/javascripts/nav.js
@@ -16,15 +16,40 @@ const handleSide = () => {
   sideMenu.classList.add('activeSide');
 };
 
+const handleMobile = () => {
+  const mbMenu = document.getElementsByClassName('mbMenu')[0];
+  const mbLogo = document.getElementsByClassName('mbLogo')[0];
+  const mbSide = document.getElementsByClassName('mbSide')[0];
+
+  if (mbMenu.classList.value.indexOf('Active') !== -1) {
+    mbMenu.classList.remove('mbMenuActive');
+  } else {
+    mbMenu.classList.add('mbMenuActive');
+  }
+
+  if (mbLogo.classList.value.indexOf('Active') === -1) {
+    mbLogo.classList.add('mbLogoActive');
+    mbSide.style.top = '2rem';
+  } else {
+    mbLogo.classList.remove('mbLogoActive');
+  }
+};
+
 const setListener = () => {
   const topLogo = document.getElementsByClassName('logo')[0];
   const sideLogo = document.getElementsByClassName('logo')[1];
+  const mbLogo = document.getElementsByClassName('mbLogo')[0];
+  const mbClose = document.getElementsByClassName('mbClose')[0];
 
   topLogo.removeEventListener('click', handleTop);
   sideLogo.removeEventListener('click', handleSide);
+  mbLogo.removeEventListener('click', handleMobile);
+  mbClose.removeEventListener('click', handleMobile);
 
   topLogo.addEventListener('click', handleTop);
   sideLogo.addEventListener('click', handleSide);
+  mbLogo.addEventListener('click', handleMobile);
+  mbClose.addEventListener('click', handleMobile);
 };
 
 setListener();
