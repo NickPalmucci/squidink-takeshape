853f20ef6bf7670672896a5a9920e54f79043722
Mucciano <nicolas.eli.palmucci@gmail.com>
2019-08-15 14:59:42 -0400

    comment out all scroll and nav logic

--- src/javascripts/scroll/mobile.js
+++ src/javascripts/scroll/mobile.js
@@ -1,17 +1,17 @@
-
-export default (sm, controller) => {
-  console.log('hit one');
-  new sm.Scene({duration: 0})
-    .setPin('.mbSide')
-    .addTo(controller);
-
-  const sceneTwo = new sm.Scene({triggerElement: '#trigger'})
-    .setClassToggle('.mbLogo', 'mbLogoActive')
-    .addTo(controller);
-
-  sceneTwo.on('enter', () => {
-    console.log('hit two');
-    const mbMenu = document.getElementsByClassName('mbSide')[0];
-    mbMenu.style.top = '2rem';
-  });
-};
+//
+// export default (sm, controller) => {
+//   console.log('hit one');
+//   new sm.Scene({duration: 0})
+//     .setPin('.mbSide')
+//     .addTo(controller);
+//
+//   const sceneTwo = new sm.Scene({triggerElement: '#trigger'})
+//     .setClassToggle('.mbLogo', 'mbLogoActive')
+//     .addTo(controller);
+//
+//   sceneTwo.on('enter', () => {
+//     console.log('hit two');
+//     const mbMenu = document.getElementsByClassName('mbSide')[0];
+//     mbMenu.style.top = '2rem';
+//   });
+// };
--- src/javascripts/scroll/side.js
+++ src/javascripts/scroll/side.js
@@ -1,15 +1,15 @@
-
-export default (sm, controller) => {
-  new sm.Scene({duration: 0})
-    .setPin('.sideMenu')
-    .addTo(controller);
-
-  const sceneOne = new sm.Scene({triggerElement: '#trigger'})
-    .setClassToggle('.sideMenu', 'displayMenu')
-    .addTo(controller);
-
-  sceneOne.on('enter', () => {
-    const sideMenu = document.getElementsByClassName('sideMenu')[0];
-    sideMenu.style.top = '2rem';
-  });
-};
+//
+// export default (sm, controller) => {
+//   new sm.Scene({duration: 0})
+//     .setPin('.sideMenu')
+//     .addTo(controller);
+//
+//   const sceneOne = new sm.Scene({triggerElement: '#trigger'})
+//     .setClassToggle('.sideMenu', 'displayMenu')
+//     .addTo(controller);
+//
+//   sceneOne.on('enter', () => {
+//     const sideMenu = document.getElementsByClassName('sideMenu')[0];
+//     sideMenu.style.top = '2rem';
+//   });
+// };
